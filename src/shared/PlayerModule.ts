2bd28b717c781352598fa55754632f67721e95e0
yhari <tuan29102005@gmail.com>
2025-05-19 15:46:27 +0700

    animations

--- src/shared/PlayerModule.ts
+++ src/shared/PlayerModule.ts
@@ -2,6 +2,9 @@ export class PlayerModule {
     static WalkAnimationId = "rbxassetid://100518044227402";
     static IdleAnimationId = "rbxassetid://122386052443389";
     static IdleAnimationIdLong = "rbxassetid://122386052443389";
+    static RunAnimationId = "rbxassetid://102741245577948";
+    static JumpAnimationId = "rbxassetid://139010106787154";
+    static FallAnimationId = "rbxassetid://87641686430040";
 
     static WalkSpeed = 7;
     static RunSpeed = 16;
